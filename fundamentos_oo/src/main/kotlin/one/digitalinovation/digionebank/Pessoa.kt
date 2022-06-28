c970d16e1e396e7853fb804294cacbe3fc877ecc
Cleyson1984 <cleyson.romero@gmail.com>
2022-06-27 22:16:36 -0300

    created person class

---
+++ fundamentos_oo/src/main/kotlin/one/digitalinovation/digionebank/Pessoa.kt
@@ -0,0 +1,12 @@
+package one.digitalinovation.digionebank
+
+class Pessoa {
+    var NOME : String = "Cleyson"
+    var CPF : String = "133.133.122.34"
+}
+
+fun main() {
+    val Cleyson = Pessoa()
+    println(Cleyson.NOME)
+    println(Cleyson.CPF)
+}
